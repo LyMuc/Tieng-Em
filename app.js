2aefe006bf1c26f632e2d6272c267d87127a802b
quannh204 <Quan.NH2710@gmail.com>
2025-04-23 22:55:44 +0700

    sua lai file signin, signup

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const pool = require("./db/db"); //Import pool tu db.js, de su sung postgresql
 
 const app = express();
@@ -9,12 +10,20 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/users', async (req, res) => {
   try {
-    const {rows} = await pool.query('Select * from users');
+    const { rows } = await pool.query('Select * from users');
     res.json(rows);
   } catch (err) {
     console.log(err);
     res.status(500).send('Server error');
   }
+});
+
+app.get('/signin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'static', 'html', 'signin.html'));
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'static', 'html', 'signup.html'));
 });
 
 app.listen(PORT, () => {
